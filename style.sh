87e21559ebc2708ab31cf3dcc4b7944783bab93c
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-23 02:47:42 +0700

    Create style.sh

---
+++ style.sh
@@ -0,0 +1,109 @@
+JANGAN BANYAK BACOT
+#EH ANJING MAU NGERECODE YA
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo " ╔═════════════════════════$cyan═╦═══════════════════════════╗"
+echo " ║ 1.DARK FB               $cyan ║ 2.TOOLS HACK FB           ║"
+echo " ║ 3.HACK FF               $cyan ║ 4.TOOLS BAJINGAN          ║"
+echo " ║ 5.MBF                   $cyan ║ 6.DARK FB GOLD            ║"
+echo " ║ 7.SPAM                  $cyan ║ 8.AUTO LIKE FB            ║"
+echo " ║ 9.EXPLOIT               $cyan ║ 10.KELUAR                 ║"
+echo "  ════════════════════════$cyan══╦═══════════════════════════"
+echo
+read -p "PILIH : " broif [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 1
+clear
+echo "$cyan SABAR TOD OTW INSTALL TOOLS HACK FB"
+echo "$cyan TUNGGU 5 DETIK"
+sleep 1
+git clone https://github.com/TimeEror123/Darknew
+cd Darknew
+python2 DARKFB.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo "$cyan SCRIPT MASI DALAM PROSES"
+echo "$cyan SILAKAN GUNAKAN SCRIPT LAIN"
+echo
+read -p "{KEMBALI}"
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+echo "$cyan SCRIPT MASI DALAM PROSES"
+echo "$cyan SILAKAN GUNAKAN SCRIPT LAIN"
+echo
+read -p "{KEMBALI}"
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+echo "OTW CUKK SABAR... :)"
+sleep 1
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+cowsay -f eyes "!!!"
+echo "$cyan SABAR TOD OTW INSTALL SCRIPT BAJINGAN TOOLS"
+git clone https://github.com/storiku/MBF
+cd MBF
+python2 mbf.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+echo "SABAR TOD OTW INSTALL SCRIPT DARK FB GOLD"|lolcat
+git clone https://github.com/TechnicalAli433/Dark-FB-Gold
+cd Dark-FB-Gold
+python2 Dark-FB-Gold.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+echo "NGAPAIN LU ASWW !!!"
+git clone https://github.com/storiku/spammerWA
+cd spammerWA
+python2 cwa.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+echo "OTW COK..."
+sleep
+git clone https://github.com/VanGanteng/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+echo "SABAR CUK TUNGGU 5 DETIK EH 1JAM EHH 1 DETIK WKWKWK:)"
+sleep
+git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsApp
+chmod +x HxWhatsApp
+python2 HxWhatsApp.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+echo "$purple TERIMA KASIH TELAH MENGGUNAKAN TOOLS KAMI"
+echo "$cyan BY : FAJAR CYBER "
+echo
+fi
